f2e553be3660a3071752437739be5ee2d6c05f36
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-05-26 09:45:44 +0200

    chore: format

--- shell/lib.ts
+++ shell/lib.ts
@@ -8,9 +8,9 @@ export const writeLoaderSync = (
 	data: Bindings | ProjectBindings,
 ) => {
 	const i =
-		category === 'shaderloader' ?
-			`import type { ProjectBindings } from '../../bindings.js';\n\nexport const ${name}Bindings: ProjectBindings = `
-		:	`import type { Bindings } from '../../bindings.js';\n\nexport const ${name}Bindings: Bindings = `;
+		category === 'shaderloader'
+			? `import type { ProjectBindings } from '../../bindings.js';\n\nexport const ${name}Bindings: ProjectBindings = `
+			: `import type { Bindings } from '../../bindings.js';\n\nexport const ${name}Bindings: Bindings = `;
 
 	const text = `${i + JSON.stringify(data, null, '\t').replaceAll(`"`, `'`)};\n`;
 
